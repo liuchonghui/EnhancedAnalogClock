a08c7cb6b54bf8d360d2736f487d1747f70bc7cd
liuchonghui <liuchonghui@139.com>
2014-02-21 09:16:59 +0800

    update
    update

---
+++ project/src/com/solo/widget/enhancedanalogclock/EnhancedAnalogClockDemo.java
@@ -0,0 +1,16 @@
+package com.solo.widget.enhancedanalogclock;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * @author liu_chonghui
+ *
+ */
+public class EnhancedAnalogClockDemo extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
